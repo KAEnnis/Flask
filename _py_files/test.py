e904ca1473b6012bc045b593738c1e44f3d90ebc
robteach <robert.cleary.teach@gmail.com>
2024-03-05 21:05:26 +0000

    update demo04.ipynb session edits and add .py files for running py files section

---
+++ _py_files/test.py
@@ -0,0 +1,3 @@
+from test_package import test_module
+print("this is test.py")
+test_module.greet("rob")
---
+++ _py_files/test_package/test_module.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("Hi there:", name)
+
+if __name__ == "__main__": #stops below from executing unless executing this file
+    greet("test") #call function
