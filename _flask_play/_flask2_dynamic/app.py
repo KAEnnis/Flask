fc29ba2bab1c2bd5550994e5fb41fba412c54981
robteach <robert.cleary.teach@gmail.com>
2024-03-12 18:45:09 +0000

    add ./_flask_play folder and sub-folders for _py_files ./_py_files/_01.../ and _02.../

---
+++ _flask_play/_flask2_dynamic/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/greet/")
+@app.route("/greet/<a_name>")
+def greet(a_name="anonymous"):
+    return f"<h1> Hello: greetings from {a_name} <h1>" 
+
+
+@app.route("/digitSum/<int:a_num>")                      
+def digitSum(a_num):    
+    sum = 0
+    while (digit := a_num%10):
+        sum += digit
+        a_num = a_num//10
+
+    return f"<h2> Sum or digits in URL num received is: {sum}<h2>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
